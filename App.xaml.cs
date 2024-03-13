f80453415bc6771b86f69fa5461d786b5c5054c0
KarKinTal <100282563+KarKinTal@users.noreply.github.com>
2024-03-13 14:56:52 +0100

    Dodaj pliki projektów.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿//using AndroidGun.Services;
+//using AndroidGun.UI;
+namespace AndroidGun
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Views/HomePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace AndroidGun.Views;
+
+public partial class HomePage : ContentPage
+{
+	public HomePage()
+	{
+		InitializeComponent();
+	}
+}
