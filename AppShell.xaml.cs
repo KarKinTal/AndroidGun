aedd9980a00c7f988adcd7ea45469086ded8c8be
167433 <100282502+167433@users.noreply.github.com>
2024-04-14 22:37:30 +0200

    Add files via upload

---
+++ AppShell.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace AndroidGunFinal
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(Wydanie), typeof(Wydanie));
+        }
+        private async void GoToMenu(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Menu());
+        }
+    }
+}
---
+++ MauiProgram.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Logging;
+using ZXing.Net.Maui.Controls;
+
+namespace AndroidGunFinal
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                })
+                .UseBarcodeReader();
+
+            builder.Services.AddTransient<MainPage>();
+
+            builder.Services.AddTransient<Wydanie>();
+
+            builder.Services.AddTransient<EtykietaInfo>();
+
+            builder.Services.AddTransient<Menu>();
+
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
