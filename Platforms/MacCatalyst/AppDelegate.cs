b038d5c6ba65d289c45f272b4f50a64018d30d17
167433 <100282502+167433@users.noreply.github.com>
2024-04-14 22:39:02 +0200

    Add files via upload

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AndroidGunFinal
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
