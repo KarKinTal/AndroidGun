e4a149e29ebd8206a7b58492493241c1d692f257
167433 <100282502+167433@users.noreply.github.com>
2024-05-07 21:57:15 +0200

    poprawione widoki

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,32 +1,45 @@
 ﻿using ZXing.Net.Maui.Controls;
 using ZXing.Net.Maui;
 using ZXing.Net.Maui.Readers;
+using AndroidGunFinal.Models;
 
 namespace AndroidGunFinal
 {
     public partial class MainPage : ContentPage
     {
+        public string magazyn { get; set; } = "magazyn";
+        public string sektor = "";
+        public string Uwagi = "";
         public MainPage()
         {
             InitializeComponent();
+
             barcodeReader.Options = new ZXing.Net.Maui.BarcodeReaderOptions
             {
                 Formats = ZXing.Net.Maui.BarcodeFormat.Code128,
                 AutoRotate = true,
                 Multiple = true
+                
             };
             barcodeReader.CameraLocation = ZXing.Net.Maui.CameraLocation.Rear;
         }
+        
+        public LoginViewModel vm;
         private void barcodeReader_BarcodeDetected(object sender, ZXing.Net.Maui.BarcodeDetectionEventArgs e)
         {
             var first = e.Results?.FirstOrDefault();
-            if (first != null) { return; }
-            Dispatcher.DispatchAsync(async () => {
-            await DisplayAlert("Barcode detected", first.Value, "Ok");
+            // DictClass.slownik.Add("key", "value");
+            if (first is null) { return; }
+
+            Dispatcher.Dispatch(() =>
+            {
+                MagazynLbl.Text = first.Value;
+                SektorLbl.Text = "Sektor";
+                UwagiLbl.Text = "Uwaga";
+                
             });
+        
         }
-
-
     }
 
 }
