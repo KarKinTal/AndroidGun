3e2d1e75f385ba9afac5ad1c9a57a4fbd81c9e9e
167433 <100282502+167433@users.noreply.github.com>
2024-05-22 19:21:58 +0200

    Add files via upload

---
+++ Login.xaml.cs
@@ -0,0 +1,55 @@
+﻿
+using System.Text;
+using AndroidGunFinal.Services;
+using Newtonsoft.Json;
+namespace AndroidGunFinal;
+
+public partial class Login : ContentPage
+{
+    HttpClient client = new HttpClient();
+
+    public Login()
+	{
+        InitializeComponent();
+        client.BaseAddress = new Uri("http://localhost:8080/");
+    }
+
+
+    public string DoLogin()
+    {
+        try
+        {
+            var login = new LoginClass(PasswordTxtIn.Text, loginTxtIn.Text);
+            string jsonData = JsonConvert.SerializeObject(login);
+            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var response =  client.PostAsync("api/auth/login", content);
+            var jsonRespone =  response.Result;
+
+            if (jsonRespone.IsSuccessStatusCode)
+            {
+                PasswordTxtIn.Text = "test";
+                loginTxtIn.Text = "test";
+                return jsonRespone.StatusCode.ToString();
+            }
+            else
+            {
+                PasswordTxtIn.Text = "Błąd";
+                loginTxtIn.Text = "Błąd";
+                return jsonRespone.StatusCode.ToString();
+
+            }
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+
+    }
+    public async void MoveToApp(System.Object sender, System.EventArgs e)
+    {
+        await Task.Run(DoLogin);
+        
+         Navigation.PushAsync(new MainPage());
+
+    }
+}
---
+++ LoginClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AndroidGunFinal.Services
+{
+    public class LoginClass
+    {
+        public LoginClass(string pass, string login)
+        {
+            this.pass = pass;
+            this.login = login;
+        }
+
+        public string pass { get; set; }
+        public string login { get; set; }
+    }
+
+
+}
